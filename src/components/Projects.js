373aff25e887a515c76a8f9b5f161f21d63c2668
Eswarkartheekgrandhi <20pa1a0554@vishnu.edu.in>
2023-03-16 15:49:14 +0530

    Added Education

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
+import "../styles/Education.css";
+import logo from "../images/vishnu.png";
 
 function Projects() {
   return (
     <div>
-      Projects
-      <Footer />
+      <div className="education_container">
+        <div className="education_top">
+          <h2>Vishnu Institute of Technology</h2>
+        </div>
+        <div className="education_body">
+          <img src={logo} alt="" />
+          <p>
+            I have learnt Technologies such as Artificial Intelligence, Machine
+            Learning, Data Science, Full Stack Development and Programming
+            Languages such as Python, Java, C, JavaScript, PHP and Frameworks
+            such as Django, React JS, Angular, JQuery, Express JS, Mangoose
+          </p>
+          <a href="https://vishnu.edu.in/" target="_blank">
+            Visit Website
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
